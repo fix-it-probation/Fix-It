1017959c7b6de2f3029947ac5787d926ebc5b50c
hidayattaufiqur <htaufiqurrahma2001@gmail.com>
2021-08-16 22:53:19 +0800

    makes some changes on mysql connection

--- app/helpers/db.js
+++ app/helpers/db.js
@@ -6,7 +6,10 @@ const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB
+    database: process.env.DB,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 // open the MySQL connection
