84e563a478b6ac89241fc5739bf24b0a19df34ee
farishisyamm <xxia321@gmail.com>
2021-08-28 01:16:19 +0700

    Get Data Pending Mitra and Feed

--- fixit/src/Screens/HomeAdmin/component.js
+++ fixit/src/Screens/HomeAdmin/component.js
@@ -1,13 +1,51 @@
-import React from 'react'
-import { View, Text, Image } from 'react-native'
+import React from 'react';
+import {View, Text, Image} from 'react-native';
 import CardAdmin from '../../components/CardAdmin/component';
 import IMAGES from '../../configs';
-import { COLOR_WHITE } from '../../styles';
+import {COLOR_WHITE} from '../../styles';
 import styles from './styles';
 
-const HomeAdmin = ({ labelMitra, labelFeed, navigation }) => {
-  labelMitra = "10"
-  labelFeed = "4"
+const HomeAdmin = ({navigation}) => {
+  const [label, setLabel] = useState({
+    mitra: '',
+    feed: '',
+  });
+
+  const GetPendingService = async () => {
+    try {
+      const res = await baseURL.get('/services/pending');
+      const dataPendingMitra = res.data[0]['Verifikasi Mitra'];
+      setLabel({
+        ...label,
+        mitra: dataPendingMitra,
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const GetPendingFeed = async () => {
+    try {
+      const res = await baseURL.get('/feeds/pending');
+      const dataPendingFeed = res.data[0]['Ads Verification'];
+      setLabel({
+        ...label,
+        feed: dataPendingFeed,
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  const GetPending = () => {
+    GetPendingFeed();
+    GetPendingService();
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      GetPending();
+    }, 10);
+  }, []);
+
   return (
     <View style={{backgroundColor: COLOR_WHITE, flex: 1}}>
       <View style={styles.card}>
@@ -15,15 +53,27 @@ const HomeAdmin = ({ labelMitra, labelFeed, navigation }) => {
         <Text style={styles.textProfile}>Admin</Text>
       </View>
       <Text style={styles.textHeader}>Penting hari ini</Text>
-      <Text style={styles.textBody}>Aktivitas yang perlu kamu pantau untuk jaga kepuasan user</Text>
+      <Text style={styles.textBody}>
+        Aktivitas yang perlu kamu pantau untuk jaga kepuasan user
+      </Text>
       <View style={{flexDirection: 'row'}}>
-        <CardAdmin onPress={() => navigation.navigate('VerifyMitra')}
-        images={IMAGES.verifyMitra} title="Verifikasi Mitra" label={labelMitra} customContainer={styles.cardMitra} />
-        <CardAdmin onPress={() => navigation.navigate('VerifyFeed')}
-        images={IMAGES.verifyFeed} title="Verifikasi Feed" label={labelFeed} customContainer={styles.cardFeed} />
+        <CardAdmin
+          onPress={() => navigation.navigate('VerifyMitra')}
+          images={IMAGES.verifyMitra}
+          title="Verifikasi Mitra"
+          label={label.mitra}
+          customContainer={styles.cardMitra}
+        />
+        <CardAdmin
+          onPress={() => navigation.navigate('VerifyFeed')}
+          images={IMAGES.verifyFeed}
+          title="Verifikasi Feed"
+          label={label.feed}
+          customContainer={styles.cardFeed}
+        />
       </View>
     </View>
-  )
-}
+  );
+};
 
 export default HomeAdmin;
